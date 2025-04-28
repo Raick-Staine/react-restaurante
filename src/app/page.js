0f408aaff33c2598ce9c1784b390a942c4dbac6f
Raick Staine <raickstaine@gmail.com>
2025-04-28 17:33:39 -0300

    Adicionando filter e cards

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,28 @@
 import styles from "./page.module.css";
 import Categorias from "@/components/Categorias";
 import { useState } from "react";
+import Search from "@/components/Search";
+import Card from "@/components/Card";
+import { filterProducts, searchProducts } from "@/Service";
+
 
 export default function Home() {
 
   const [btnClicked, setBtnCliked] = useState('Entradas');
+  const [searchText, setSearchText] = useState('');
+  const [list, setList] = useState(filterProducts('Entradas'));
 
   const handleFilter = (category) => {
-    setBtnCliked(category)
+    setList(filterProducts(category));
+    setBtnCliked(category);
+    setSearchText('');
+  }
+
+  const handleSearch = (typedText) => {
+    setSearchText(typedText);
+    if (typedText.length > 2) {
+      setList(searchProducts(typedText));
+    }
   }
 
   return (
@@ -17,9 +32,37 @@ export default function Home() {
       <section className={styles.banner}></section>
 
       <main className={styles.main}>
-        <Categorias onClickEntradas={() => handleFilter('Entradas')} selectedBTn={btnClicked} />
+
+        <Categorias
+          onClickEntradas={() => handleFilter('Entradas')}
+          onClickMassas={() => handleFilter('Massas')}
+          onClickCarnes={() => handleFilter('Carnes')}
+          onClickBebidas={() => handleFilter('Bebidas')}
+          onClickSaladas={() => handleFilter('Saladas')}
+          onClickSobremesas={() => handleFilter('Sobremesas')}
+          selectedBtn={btnClicked} />
+
+        <Search onChange={(event) => handleSearch(event.target.value)}
+          value={searchText} />
+
+        <section className={styles.foodMenu}>
+          <h2>Cardápio</h2>
+          <div className={styles.cards}>
+            {list.map((product) => (
+              <Card
+                key={product.id}
+                name={product.name}
+                image={product.image}
+                category={product.category}
+                description={product.description}
+                price={product.price}
+                length={list.length}
+              />
+            ))}
+          </div>
+        </section>
       </main>
-      
+
     </div>
   );
 }
--- src/components/Categorias/index.js
+++ src/components/Categorias/index.js
@@ -1,47 +1,45 @@
 import styles from "./Categorias.module.css";
 import Image from "next/image";
 
-import Entrada from "../../../public/entrada.png";
-import Massa from "../../../public/massa.png";
-import Carne from "../../../public/carne.png";
+import Entradas from "../../../public/entrada.png";
+import Massas from "../../../public/massa.png";
+import Carnes from "../../../public/carne.png";
 import Bebidas from "../../../public/bebidas.png";
-import Salada from "../../../public/salada.png";
-import Sobremesa from "../../../public/sobremesa.png";
+import Saladas from "../../../public/salada.png";
+import Sobremesas from "../../../public/sobremesa.png";
 
 export default function Categorias(props) {
-    const handlerSelectedBtn = (category) => {
-        props.selectedBtn == category && styles.selectedBtn;
-    }
-
+    const handleSelectedBtn = (category) => props.selectedBtn == category && styles.selectedBtn;
+    
     return (
-        <section className={styles.Categories}>
-            <button onClick={props.onClickEntradas} className={`${styles.btn} ${handlerSelectedBtn('Entradas')}`}>
-                <Image src={Entrada} alt='Prato de entrada' />
+        <section className={styles.categories}>
+            <button onClick={props.onClickEntradas} className={`${styles.btn} ${handleSelectedBtn('Entradas')}`}>
+                <Image src={Entradas} alt='Prato de entrada' />
                 <span>Entradas</span>
             </button>
 
-            <button onClick={props.onClickMassa} className={`${styles.btn} ${handlerSelectedBtn('Massas')}`}>
-                <Image src={Massa} alt='Prato de entrada' />
+            <button onClick={props.onClickMassas} className={`${styles.btn} ${handleSelectedBtn('Massas')}`}>
+                <Image src={Massas} alt='Prato de entrada' />
                 <span>Massas</span>
             </button>
 
-            <button onClick={props.onClickCarne} className={`${styles.btn} ${handlerSelectedBtn('Carnes')}`}>
-                <Image src={Carne} alt='Prato de entrada' />
+            <button onClick={props.onClickCarnes} className={`${styles.btn} ${handleSelectedBtn('Carnes')}`}>
+                <Image src={Carnes} alt='Prato de entrada' />
                 <span>Carnes</span>
             </button>
 
-            <button onClick={props.onClickBebidas} className={`${styles.btn} ${handlerSelectedBtn('Bebidas')}`}>
+            <button onClick={props.onClickBebidas} className={`${styles.btn} ${handleSelectedBtn('Bebidas')}`}>
                 <Image src={Bebidas} alt='Prato de entrada' />
                 <span>Bebidas</span>
             </button>
 
-            <button onClick={props.onClickSalada} className={`${styles.btn} ${handlerSelectedBtn('Saladas')}`}>
-                <Image src={Salada} alt='Prato de entrada' />
+            <button onClick={props.onClickSaladas} className={`${styles.btn} ${handleSelectedBtn('Saladas')}`}>
+                <Image src={Saladas} alt='Prato de entrada' />
                 <span>Saladas</span>
             </button>
 
-            <button onClick={props.onClickSobremesa} className={`${styles.btn} ${handlerSelectedBtn('Sobremesas')}`}>
-                <Image src={Sobremesa} alt='Prato de entrada' />
+            <button onClick={props.onClickSobremesas} className={`${styles.btn} ${handleSelectedBtn('Sobremesas')}`}>
+                <Image src={Sobremesas} alt='Prato de entrada' />
                 <span>Sobremesas</span>
             </button>
 
